3256fa8d609cae3b1da5ccdd911b33d31dc0ee48
Edward Hesketh <headblockhead@gmail.com>
2023-04-01 20:45:39 +0100

    moved all files to flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,27 @@
         pkgs = import nixpkgs {
       system = "x86_64-linux";
     };
-    easyeda-router = pkgs.callPackage ./default.nix { };
+    easyeda-router = pkgs.stdenv.mkDerivation {
+  pname = "easyeda-router";
+  version = "0.8.11";
+  src = pkgs.fetchurl {
+    url = "https://image.easyeda.com/files/easyeda-router-linux-x64-v0.8.11.zip";
+    sha256 = "GIxxq93Cmr2lyqtufrsVjBuqRd/OxuDhjxUKJTzeE4M=";
+  };
+  buildInputs = [ pkgs.jdk ];
+  installPhase = ''
+  mkdir -p $out/bin
+  ln -sf ${pkgs.jdk}/bin/java $out/bin/java
+    printf "$out/bin/java -XX:+UseG1GC -Dcom.easyeda.env=local -jar $out/opt/easyeda-router-linux-x64-v0.8.11/bin/bootstrap.jar" > $out/bin/easyeda-router
+    chmod +x $out/bin/easyeda-router
+  '';
+  unpackPhase = ''
+  mkdir $out
+  mkdir $out/opt
+  jar xvf $src
+  mv easyeda-router-linux-x64-v0.8.11 $out/opt
+  '';
+};
   in
   {
     packages.x86_64-linux.easyeda-router = easyeda-router;
--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-let 
-  easyeda-router = import ./default.nix { inherit pkgs; };
-in
-  pkgs.mkShell {
-    nativeBuildInputs = [ easyeda-router ];
-}
